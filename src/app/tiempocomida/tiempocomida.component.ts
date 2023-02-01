1e49bf4823013caf33ed5e4a8b7785c85eadd5ae
sjchalini <silviana.jchalini@gmail.com>
2023-01-31 19:29:47 -0600

    Cambios en la intefarz de registro de usuarios

--- src/app/tiempocomida/tiempocomida.component.ts
+++ src/app/tiempocomida/tiempocomida.component.ts
@@ -10,6 +10,21 @@ export class TiempocomidaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
+  }
+
+  name = 'identificadorPaciente.json';
+  exportName(): void {
+    let element = document.getElementById('idInputIdentificaPaciente');
+    //String IdentificadorPAciente="";
+
+
+    //const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    //const book: XLSX.WorkBook = XLSX.utils.book_new();
+    //XLSX.utils.book_append_sheet(book, worksheet, 'Sheet1');
+
+    //XLSX.writeFile(book, this.name);
   }
 
 }
